3b175cc38fa61e193be6a5f6252310411c343f2c
Inomjon Ismaylov <ismoilov3600@gmail.com>
2023-12-03 20:49:26 +0500

    npm, webpack, ts config

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.ts",
+
+  output: {
+    filename: "bundle.js",
+
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+
+        use: "ts-loader",
+
+        exclude: /node_modules/,
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+
+  devtool: "source-map",
+
+  devServer: {
+    // contentBase: path.join(__dirname, "dist"),
+
+    static: './',
+
+    compress: true,
+
+    port: 9000,
+  },
+
+  mode: "development",
+};
